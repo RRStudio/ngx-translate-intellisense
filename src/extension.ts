f068c882ff86348b190cb3711de804737a80113d
r0nsha <44722426+r0nsha@users.noreply.github.com>
2020-10-22 16:17:35 +0300

    translation works!

--- src/extension.ts
+++ src/extension.ts
@@ -2,21 +2,37 @@ import { existsSync, promises, readdir, readFile, readFileSync } from "fs";
 import * as path from "path";
 import * as vscode from "vscode";
 
-// TODO: suggest translations from arbitrary array
 // TODO: suggest translations from json file
 // TODO: translated string highlighting
 // TODO: translated string show translations on hover
-// TODO: automatic create translation of selection
+// TODO: create translation of selection
+// TODO: show option to create translation of inserted string after prefix
 
 const NAME = "ngx-translate-intellisense";
-const output = vscode.window.createOutputChannel(NAME);
+const completionPrefix = "t:";
+let output: vscode.OutputChannel;
+
+const translationsFolderName = "i18n";
+const translationFileExtension = "json";
+
+let translationFiles: string[] = [];
+let translations: unknown[] = [];
+let languages: string[] = [];
 
 function write(message: string) {
   output.appendLine(message);
 }
 
 export function activate(context: vscode.ExtensionContext) {
-  context.subscriptions.push(completionTranslations(), commandRonini());
+  output = vscode.window.createOutputChannel(NAME);
+  indexTranslations()
+    .then((result) => {})
+    .catch((error) => {});
+
+  context.subscriptions.push(
+    completionTranslations(),
+    commandUpdateTranslations()
+  );
 }
 
 // this method is called when your extension is deactivated
@@ -24,10 +40,12 @@ export function deactivate() {}
 
 function completionTranslations(): vscode.Disposable {
   return vscode.languages.registerCompletionItemProvider(
-    {
-      scheme: "file",
-      language: "typescript",
-    },
+    [
+      {
+        scheme: "file",
+        language: "html",
+      },
+    ],
     {
       provideCompletionItems(
         document: vscode.TextDocument,
@@ -35,77 +53,118 @@ function completionTranslations(): vscode.Disposable {
         token: vscode.CancellationToken,
         context: vscode.CompletionContext
       ) {
-        const key = "key";
-        const values = ["value"];
-        return [
-          {
-            kind: vscode.CompletionItemKind.Constant,
-            label: key,
-            detail: `Translations from '${key}'`,
-            documentation: new vscode.MarkdownString(
-              `**Key:** ${key}  \n  **EN:** ${values[0]}`
-            ),
-          },
-        ];
+        if (translationFiles.length === 0 || translations.length === 0) {
+          return null;
+        } else {
+          const defaultTranslation = translations[0];
+          const items = Object.keys(defaultTranslation)
+            .map((key, index) => {
+              if (defaultTranslation.hasOwnProperty(key)) {
+                const values = ["value"];
+                let documentationText = "";
+
+                for (let i = 0; i < languages.length; i++) {
+                  documentationText += `**${languages[i].toUpperCase()}:** ${
+                    translations[i][key]
+                  }  \n  `;
+                }
+
+                return {
+                  kind: vscode.CompletionItemKind.Constant,
+                  label: completionPrefix + key,
+                  insertText: `{{ '${key}' | translate }}`,
+                  detail: `Translation for '${key}'`,
+                  documentation: new vscode.MarkdownString(documentationText),
+                };
+              } else {
+                return null;
+              }
+            })
+            .filter((i) => i !== null);
+          return items;
+        }
       },
     }
   );
 }
 
-const translationsFolderName = "i18n";
-const translationFileExtension = "json";
-const defaultLanguage = "";
 let dirs: string[] = [];
 
-function commandRonini(): vscode.Disposable {
-  return vscode.commands.registerCommand(`${NAME}.ronini`, async () => {
-    try {
-      write("searching workspace...");
-      const folders = vscode.workspace.workspaceFolders;
-      if (folders?.length === 0) {
-        return;
-      }
-      dirs = [];
-      for (const f of folders) {
-        await listDirectoriesRecursive(f.uri.fsPath + "/src");
-      }
-      dirs = dirs.filter((d) => {
-        return d.endsWith(translationsFolderName);
-      });
-      if (dirs.length > 0) {
-        const dir = dirs[0];
-        write(`found ${translationsFolderName} directory (${dir})...`);
-        write("searching for a translation file...");
-        let translationFile: string;
-        if (defaultLanguage !== null && defaultLanguage.trim() !== "") {
-          translationFile = path.join(
-            dir,
-            `${defaultLanguage}.${translationFileExtension}`
-          );
-        } else {
-          let translationFiles = await listFiles(dir);
-          translationFiles = translationFiles.filter((f) => {
-            return f.endsWith("." + translationFileExtension);
+function commandUpdateTranslations(): vscode.Disposable {
+  return vscode.commands.registerCommand(
+    `${NAME}.commandUpdateTranslations`,
+    async () => {
+      try {
+        vscode.window.showInformationMessage("Updating translations");
+        indexTranslations()
+          .then((result) => {
+            vscode.window.showInformationMessage(
+              "Updated translations successfully"
+            );
+          })
+          .catch((error) => {
+            vscode.window.showInformationMessage(
+              "Failed updating translations"
+            );
           });
-          translationFile = translationFiles[0];
-        }
-
-        if (!(await existsSync(translationFile))) {
-          vscode.window.showErrorMessage(
-            `Couldn\'t find translation file: ${translationFile}`
-          );
-        } else {
-          write(`found translation file (${translationFile})`);
-          const fileBuffer = readFileSync(translationFile);
-          const json = JSON.parse(fileBuffer);
-          write(JSON.stringify(json, null, 2));
-          write(`read translation file contents`);
-        }
+      } catch (e) {
+        vscode.window.showErrorMessage(e);
       }
-    } catch (e) {
-      vscode.window.showErrorMessage(e);
     }
+  );
+}
+
+async function indexTranslations() {
+  translations = [];
+  translationFiles = await getTranslationFiles();
+  languages = translationFiles.map((f) => {
+    return path.basename(f, "." + translationFileExtension);
   });
+  translations = await Promise.all(
+    translationFiles.map((f) => {
+      return readTranslationFile(f);
+    })
+  );
+}
+
+async function getTranslationFiles(): string {
+  try {
+    write("searching workspace...");
+    const folders = vscode.workspace.workspaceFolders;
+    if (folders?.length === 0) {
+      return;
+    }
+    dirs = [];
+    for (const f of folders) {
+      await listDirectoriesRecursive(f.uri.fsPath + "/src");
+    }
+    dirs = dirs.filter((d) => {
+      return d.endsWith(translationsFolderName);
+    });
+    if (dirs.length > 0) {
+      const dir = dirs[0];
+      write(`found ${translationsFolderName} directory (${dir})...`);
+      write("searching for a translation file...");
+      let translationFiles = await listFiles(dir);
+      translationFiles = translationFiles.filter((f) => {
+        return f.endsWith("." + translationFileExtension);
+      });
+      return translationFiles;
+    }
+  } catch (e) {
+    vscode.window.showErrorMessage(e);
+  }
+}
+
+function readTranslationFile(file: string): unknown {
+  try {
+    const fileBuffer = readFileSync(file);
+    const json = JSON.parse(fileBuffer);
+    write(`read translation file contents`);
+    return json;
+  } catch (e) {
+    vscode.window.showErrorMessage(e);
+  }
 }
 
 async function listDirectoriesRecursive(dir) {
