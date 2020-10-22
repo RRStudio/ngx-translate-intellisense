1acf850ecce491e70fbf301dc311cca293f9c735
r0nsha <44722426+r0nsha@users.noreply.github.com>
2020-10-22 17:05:23 +0300

    hover translations

--- src/extension.ts
+++ src/extension.ts
@@ -2,14 +2,20 @@ import { existsSync, promises, readdir, readFile, readFileSync } from "fs";
 import * as path from "path";
 import * as vscode from "vscode";
 
-// TODO: suggest translations from json file
 // TODO: translated string highlighting
 // TODO: translated string show translations on hover
 // TODO: create translation of selection
 // TODO: show option to create translation of inserted string after prefix
 
 const NAME = "ngx-translate-intellisense";
+const selector = [
+  {
+    scheme: "file",
+    language: "html",
+  },
+];
 const completionPrefix = "t:";
+
 let output: vscode.OutputChannel;
 
 const translationsFolderName = "i18n";
@@ -30,7 +36,8 @@ export function activate(context: vscode.ExtensionContext) {
     .catch((error) => {});
 
   context.subscriptions.push(
-    completionTranslations(),
+    hoverTranslations(),
+    translationCompletions(),
     commandUpdateTranslations()
   );
 }
@@ -38,54 +45,66 @@ export function activate(context: vscode.ExtensionContext) {
 // this method is called when your extension is deactivated
 export function deactivate() {}
 
-function completionTranslations(): vscode.Disposable {
-  return vscode.languages.registerCompletionItemProvider(
-    [
-      {
-        scheme: "file",
-        language: "html",
-      },
-    ],
-    {
-      provideCompletionItems(
-        document: vscode.TextDocument,
-        position: vscode.Position,
-        token: vscode.CancellationToken,
-        context: vscode.CompletionContext
-      ) {
-        if (translationFiles.length === 0 || translations.length === 0) {
-          return null;
+function hoverTranslations(): vscode.Disposable {
+  return vscode.languages.registerHoverProvider(selector, {
+    provideHover(
+      document: vscode.TextDocument,
+      position: vscode.Position,
+      token: vscode.CancellationToken
+    ) {
+      if (translationFiles.length === 0 || translations.length === 0) {
+        return new vscode.Hover("Loading translations...");
+      } else {
+        const foundRange = document.getWordRangeAtPosition(
+          position,
+          new RegExp(/[',"].*[',"]\s\|\stranslate/g)
+        );
+        if (foundRange !== undefined) {
+          const text = document.getText(foundRange);
+          const firstQuote = text.indexOf("'");
+          const secondQuote = text.lastIndexOf("'");
+          const key = text.substring(firstQuote + 1, secondQuote);
+
+          return new vscode.Hover(getDocumentTextForTranslation(key));
         } else {
-          const defaultTranslation = translations[0];
-          const items = Object.keys(defaultTranslation)
-            .map((key, index) => {
-              if (defaultTranslation.hasOwnProperty(key)) {
-                const values = ["value"];
-                let documentationText = "";
-
-                for (let i = 0; i < languages.length; i++) {
-                  documentationText += `**${languages[i].toUpperCase()}:** ${
-                    translations[i][key]
-                  }  \n  `;
-                }
-
-                return {
-                  kind: vscode.CompletionItemKind.Constant,
-                  label: completionPrefix + key,
-                  insertText: `{{ '${key}' | translate }}`,
-                  detail: `Translation for '${key}'`,
-                  documentation: new vscode.MarkdownString(documentationText),
-                };
-              } else {
-                return null;
-              }
-            })
-            .filter((i) => i !== null);
-          return items;
+          return null;
         }
-      },
-    }
-  );
+      }
+    },
+  });
+}
+
+function translationCompletions(): vscode.Disposable {
+  return vscode.languages.registerCompletionItemProvider(selector, {
+    provideCompletionItems(
+      document: vscode.TextDocument,
+      position: vscode.Position,
+      token: vscode.CancellationToken,
+      context: vscode.CompletionContext
+    ) {
+      if (translationFiles.length === 0 || translations.length === 0) {
+        return null;
+      } else {
+        const defaultTranslation = translations[0];
+        const items = Object.keys(defaultTranslation)
+          .map((key, index) => {
+            if (defaultTranslation.hasOwnProperty(key)) {
+              return {
+                kind: vscode.CompletionItemKind.Constant,
+                label: completionPrefix + key,
+                insertText: `{{ '${key}' | translate }}`,
+                detail: `Translation for '${key}'`,
+                documentation: getDocumentTextForTranslation(),
+              };
+            } else {
+              return null;
+            }
+          })
+          .filter((i) => i !== null);
+        return items;
+      }
+    },
+  });
 }
 
 let dirs: string[] = [];
@@ -192,4 +211,14 @@ async function listDirectoriesRecursive(dir) {
 async function listFiles(dir) {
   const fileNames = await promises.readdir(dir);
   return fileNames.map((fileName) => path.join(dir, fileName));
+}
+
+function getDocumentTextForTranslation(key: string): vscode.MarkdownString {
+  let documentationText = "Translations:  \n  ";
+  for (let i = 0; i < languages.length; i++) {
+    documentationText += `**${languages[i].toUpperCase()}:** ${
+      translations[i][key]
+    }  \n  `;
+  }
+  return new vscode.MarkdownString(documentationText);
 }
